b1e90f56aab3dbd6834c5c7d2a1c9eb836025ca4
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-01-31 01:08:34 +0100

    Done 4:4

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,10 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+def get_squares(array)
+  array
+    .select do |elem|
+      elem if array.any? { |square| square == elem * elem }
+    end
+end
