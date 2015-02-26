da9278c32050cb36c31d741992a843f3d042992f
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-26 21:23:44 +0100

    Done 3:3

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,11 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(sentence)
+  sentence.split(' ')
+    .map(&:downcase)
+    .each_with_object({}) do |word, memo|
+      memo[word] = memo[word].to_i + 1
+    end
+end
