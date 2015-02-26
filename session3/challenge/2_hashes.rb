d58ee4dcfea42e0102760ecec8f663a8961d0f96
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-26 21:14:52 +0100

    Done 3:2

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,17 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(level)
+  odds_upto(level)
+    .each_with_object({}) do |odd_number, result|
+      result[odd_number] = evens_upto(odd_number)
+    end
+end
+
+def odds_upto(number)
+  1.upto(number).select { |num| num.odd? }
+end
+
+def evens_upto(number)
+  1.upto(number).select { |num| num.even? }
+end
