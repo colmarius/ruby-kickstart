9a6fc569fe6b72a0125f835a43d1fa79e0b90e91
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-09-02 21:11:01 +0200

    Done 4:3

--- session4/challenge/3_inject_blocks_enumerable.rb
+++ session4/challenge/3_inject_blocks_enumerable.rb
@@ -1,6 +1,6 @@
 # You are going to write a method called passthrough
 # It receives an enumerable object, and an initial passthrough value, and a block
-# 
+#
 # For each of the elements in the enumerable object,
 # it passes them the passthrough value and the element.
 # Whatever the block returns, must be passed in as the
@@ -14,7 +14,7 @@
 # passthrough 5..10 , 0 do |sum,num|
 #   sum + num
 # end
-# 
+#
 # This should return 45 in the following manner:
 #   The first time the block is passed    0 ,  5 and it returns 5
 #   The second time the block is passed   5 ,  6 and it returns 11
@@ -25,5 +25,10 @@
 #   The method then returns 45
 #
 
-def passthrough
+def passthrough(elements, initial, &block)
+  result = initial
+  elements.each do |element|
+    result = block.call(result, element)
+  end
+  result
 end
