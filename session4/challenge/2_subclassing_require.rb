7e02ed314602876ff37834322b21a3c72ab18196
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-09-02 20:50:36 +0200

    Done 4:2

--- session4/challenge/2_subclassing_require.rb
+++ session4/challenge/2_subclassing_require.rb
@@ -17,3 +17,17 @@
 # stack.push 1
 # stack.push 2
 # stack # => [1, 2]
+
+require_relative '1_stack_classes_inspect'
+
+class StackInDisguise < Stack
+  def inspect
+    node = @head
+    values = []
+    while node
+      values.push(node.data.inspect)
+      node &&= node.next_node
+    end
+    "[#{ values.reverse.join(', ') }]"
+  end
+end
